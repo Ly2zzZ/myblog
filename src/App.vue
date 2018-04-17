6178b10f3bcfeb58f729ef3f71097aa25221ed25
IamSleepy_zzZ <1093930651@qq.com>
2018-04-17 15:29:11 +0800

    log
    log

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,20 @@
       <el-container>
       <el-header>
         <my-head></my-head>
+
+          <div class="head-nav">
+          <ul class="nav-list">
+            <li> {{ username }}</li>
+            <li v-if="username!== ''" class="nav-pile">|</li>
+            <li v-if="username!== ''" @click="quit">退出</li>
+            <li v-if="username=== ''" @click="logClick">登录</li>
+            <li class="nav-pile">|</li>
+            <li v-if="username=== ''" @click="regClick">注册</li>
+            <li v-if="username=== ''" class="nav-pile">|</li>
+            <li @click="aboutClick">关于</li>
+          </ul>
+        </div> 
+
       </el-header>
       <el-main>
       <transition mode="out-in">  
@@ -16,6 +30,18 @@
       <el-footer>
         ShuaiBiYu Studying
       </el-footer>
+
+    <my-dialog :is-show="isShowAboutDialog" @on-close="closeDialog('isShowAboutDialog')">
+      <p>本报告在调研数据的基础上，采用定性与定量相结合的方式深入分析了专车市场发展的驱动因素与阻碍因素、专车市场背后的产业格局、专车企业的竞争格局、用户对专车市场的依赖程度、专车对其他交通工具运力的补充效应等，通过这五个章节的研究反映专车市场的发展态势和面临的问题。报告力求客观、深入、准确地反映中国专车市场发展情况，为政府、企事业单位和社会各界提供决策依据。 </p>
+    </my-dialog>
+    
+    <my-dialog :is-show="isShowLogDialog" @on-close="closeDialog('isShowLogDialog')">
+      <log-form @has-log="onSuccessLog"></log-form>
+    </my-dialog>
+
+    <my-dialog :is-show="isShowRegDialog" @on-close="closeDialog('isShowRegDialog')">
+      <reg-form></reg-form>
+    </my-dialog>
       </el-container>
     </el-container>
   </div>
@@ -25,14 +51,46 @@
 <script>
 import { Draw } from './assets/js.js'
 import myHead from './components/head'
+
+import Dialog from './components/dialog'
+import LogForm from './components/logForm'
+import RegForm from './components/regForm'
+
 export default {
+  name:"App",
+  components: {
+    MyDialog: Dialog,
+    LogForm,
+    RegForm,
+    myHead
+  },
   data(){
     return {
-      transitionName: 'slide-left'
+      transitionName: 'slide-left',
+        isShowAboutDialog: false,
+        isShowLogDialog: false,
+        isShowRegDialog: false,
+        username: ''
     }
   },
-  components:{
-    myHead
+  methods:{
+    logClick () {
+      this.isShowLogDialog = true
+    },
+    regClick () {
+      this.isShowRegDialog = true
+    },
+    closeDialog (attr) {
+      this[attr] = false
+    },
+    onSuccessLog (data) {
+      console.log(data)
+      this.closeDialog ('isShowLogDialog')
+      this.username = data.username
+    },
+    aboutClick () {
+      this.isShowAboutDialog = true
+    }
   },
   created: function () {
     Draw();
@@ -126,5 +184,69 @@ body{
 }
 .v-leave-active{
   transition: 0.5s;
+}
+
+
+
+
+.head-nav {
+  float: right;
+  position: absolute;
+  top:0px;
+  right: 1em;
+  font-size: 1em;
+}
+.head-nav ul {
+  overflow: hidden;
+}
+.head-nav li {
+  cursor: pointer;
+  float: left;
+}
+.nav-pile {
+  padding: 0 10px;
+}
+ol, ul {
+  list-style: none;
+}
+
+.button {
+  background: #4fc08d;
+  color: #fff;
+  display: inline-block;
+  padding: 10px 20px;
+  cursor: pointer;
+}
+.button:hover {
+  background: #4fc08d;
+}
+.g-form {
+
+}
+.g-form-line {
+  padding: 15px 0;
+}
+.g-form-label {
+  width: 100px;
+  font-size: 16px;
+  display: inline-block;
+}
+.g-form-input {
+  display: inline-block;
+}
+.g-form-input input {
+  height: 30px;
+  width: 200px;
+  line-height: 30px;
+  vertical-align: middle;
+  padding: 0 10px;
+  border: 1px solid #ccc;
+}
+.g-form-btn {
+  padding-left: 100px;
+}
+.g-form-error {
+  color: red;
+  padding-left: 15px;
 }
 </style>
